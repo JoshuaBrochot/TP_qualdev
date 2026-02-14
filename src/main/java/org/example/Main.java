6233ee26472f0bd45378a536b4853e3fc7c0444f
JoshuaBrochot <hopeshadows7@gmail.com>
2026-02-14 09:23:41 +0100

    main fonction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,40 @@
 package org.example;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
         Liste liste = new Liste();
 
-        liste.addTache("faire le tp");
-        liste.addTache("réussir");
+        Boolean flag = Boolean.TRUE;
+        String str = "";
+        while (Objects.equals(flag, Boolean.TRUE)) {
+            while (!Objects.equals(str, "1")
+                    && !Objects.equals(str, "2")
+                    && !Objects.equals(str, "3")) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("1. Ajouter une tache");
+                System.out.println("2. Lister les tache");
+                System.out.println("3. Quitter");
+                System.out.println("Veuillez saisir une action :");
+                str = sc.nextLine();
 
-        liste.voirTaches();
+                if (Objects.equals(str, "1")) {
+                    System.out.println("Veuillez saisir le nom de la tache :");
+                    String nom_tache = sc.nextLine();
+                    liste.addTache(nom_tache);
+                    System.out.println("Tache ajouté !");
+                    str = "";
+                } else if (Objects.equals(str, "2")) {
+                    liste.voirTaches();
+                    str = "";
+                } else {
+                    flag = Boolean.FALSE;
+                }
+            }
+        }
     }
 }
